3029cefe9c111b4651a7ceb96cd1d4539af341e0
Marco Maiocchi <52156326+marcomaiocchi@users.noreply.github.com>
2019-07-12 14:40:09 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,26 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1GrZLcLFzTLDG5ei7U6v47c4eZwG5q7YG'
+export_file_name = 'fashion_details_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Robe',
+'Combinaison',
+'Top',
+'Blouson',
+'Chemise',
+'Veste',
+'Blouse',
+'Tshirt',
+'Pull',
+'manchers_courtes',
+'manchers_longues',
+'sansmanchers_',
+'manchers_3/4',
+'col_rond',
+'col_montant',
+'col_chemise'
+]
 path = Path(__file__).parent
 
 app = Starlette()
